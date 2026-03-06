0bc0a1e849ada3b95e2fbc60ec40b9b24622f8da
Rufas Wan <rufaswan@gmail.com>
2026-03-06 19:17:27 +0800

    chmod

--- chmod.sh
+++ chmod.sh
@@ -1,37 +1,34 @@
 #!/bin/bash
-<<'////'
-[license]
-Copyright (C) 2019 by Rufas Wan
-
-This file is part of Web2D Games.
-    <https://github.com/rufaswan/Web2D_Games>
-
-Web2D Games is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-Web2D Games is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with Web2D Games.  If not, see <http://www.gnu.org/licenses/>.
-[/license]
-////
-
-a0=$(realpath "$0")
-d=$(dirname "$a0")
-
-function chmodfile
-{
-	case "$1" in
-		*'.sh')  chmod -c 755  "$1";;
-		*)  chmod -c 644  "$1";;
-	esac
+function chmodfile {
+	local ext="${1##*.}"
+	[[ "$ext" == sh ]] && return
+	chmod -c 644 "$1"
 }
 export -f chmodfile
 
-find "$d" -type f -print0 | xargs -0 -I {} bash -c 'chmodfile "$@"' _ {}
-find "$d" -type d -print0 | xargs -0 -I {} chmod -c 755 {}
+function chmodsh {
+	local ext="${1##*.}"
+	[[ "$ext" == sh ]] || return
+	chmod -c 755 "$1"
+}
+export -f chmodsh
+
+function chmodent {
+	[ -e "$1" ] || return
+	if [ -d "$1" ]; then
+		# BUG chmod -c UPDATE file already 644 to 644
+		# -> use find -not -perm 644 -exec chmod
+		find "$1" -type d -not -perm 755 -print0 \
+			| xargs -0 -I {} chmod -c 755 {}
+		find "$1" -type f -not -perm 644 -print0 \
+			| xargs -0 -I {} bash -c 'chmodfile "$@"' _ {}
+		find "$1" -type f -not -perm 755 -print0 \
+			| xargs -0 -I {} bash -c 'chmodsh "$@"' _ {}
+		return
+	fi
+	chmodfile "$1"
+}
+
+REAL=$(realpath "$0")
+ ENT=$(dirname "$REAL")
+chmodent "$ENT"
